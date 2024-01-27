6968d27725c0f646cda62dc154a30e14eea2dccc
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-01-27 16:14:19 +0900

    Convert to m3u8

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ FROM python:3.12-slim
 # Set the working directory in the container
 WORKDIR /usr/src/app
 
+# Install FFmpeg
+RUN apt-get update && apt-get install -y ffmpeg && rm -rf /var/lib/apt/lists/*
+
 # Install Poetry
 RUN pip install poetry
 
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
+import subprocess
 
 app = Flask(__name__)
 # Use the environment variable for UPLOAD_FOLDER if it's set, otherwise default to a local folder
 app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER', './test-video')
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 * 1024  # 1GB max-limit.
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 * 1024  # 10GB max-limit.
 
 
 # Home page route
 @app.route('/')
 def index():
     # list all videos in the UPLOAD_FOLDER, filter out non-video files
-    videos = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith(('.mp4', '.mkv', '.mov', '.webm'))]
+    videos = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith('.m3u8')]
     return render_template('index.html', videos=videos)
 
 
@@ -26,7 +27,19 @@ def upload_file():
         return redirect(request.url)
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Convert video to HLS format
+        output_m3u8 = os.path.splitext(file_path)[0] + '.m3u8'
+        segment_prefix = os.path.splitext(file_path)[0] + '_segment'
+        ffmpeg_command = [
+            'ffmpeg', '-i', file_path, '-c:v', 'libx265', '-crf', '28', '-sc_threshold', '0', '-g', '48',
+            '-keyint_min', '48', '-hls_time', '10', '-hls_playlist_type', 'vod', '-b:v', '2000k', '-maxrate', '2147k',
+            '-bufsize', '2000k', '-b:a', '128k', '-hls_segment_filename', f"{segment_prefix}%03d.ts", output_m3u8
+        ]
+        subprocess.run(ffmpeg_command)
+
         return redirect(url_for('index'))
 
 
