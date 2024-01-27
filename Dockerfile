20ed4b054f284a4bcdcadd9a7f7cd5a98c586f86
HackingGate <8541644+HackingGate@users.noreply.github.com>
2024-01-27 13:06:10 +0900

    Implement code

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Install Poetry
+RUN pip install poetry
+
+# Copy the poetry.lock and pyproject.toml files
+COPY poetry.lock pyproject.toml ./
+
+# Install dependencies but without creating virtual environments inside the Docker container
+RUN poetry config virtualenvs.create false \
+    && poetry install --no-interaction --no-ansi
+
+# Copy the rest of your application's code
+COPY . .
+
+# Expose the port the app runs on
+EXPOSE 5000
+
+# Run the app
+CMD ["python", "app.py"]
+
+# Define a volume for persistent storage
+VOLUME /mnt/media-volume
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+# Use the environment variable for UPLOAD_FOLDER if it's set, otherwise default to a local folder
+app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER', '.')
+
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024 * 1024  # 1GB max-limit.
+
+# Home page route
+@app.route('/')
+def index():
+    videos = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', videos=videos)
+
+# Upload video route
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+
+# Delete video route
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('index'))
+
+# Route for serving videos
+@app.route('/video/<filename>')
+def video(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
